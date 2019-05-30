2d67d52c5471a2bc9956159cbcc337e39b2c8dd9
Phouki <nguyen.phph@gmail.com>
2019-05-30 16:12:42 +0200

    commit navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import {
+  Container,
+  Row,
+  Col
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
 export default class NavBar extends React.Component {
   render() {
